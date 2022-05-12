22f475b9122ee3bb7547b1d76126a7260676393f
choongbin kee <cbkee@nasmedia.co.kr>
2022-05-12 22:08:05 +0900

    init

---
+++ src/main/java/com/nasmedia/adpacker/AdpackerApplication.java
@@ -0,0 +1,13 @@
+package com.nasmedia.adpacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdpackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nasmedia/adpacker/AdpackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasmedia.adpacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
